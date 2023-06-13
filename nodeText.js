10006346e89fd387b39a7bed07d4e67d1578305b
David Khachatryan <85390803+DavidKhachatryan02@users.noreply.github.com>
2023-06-13 21:36:35 +0400

    Update nodeText.js

--- nodeText.js
+++ nodeText.js
@@ -1,7 +1,9 @@
 const os = require("os");
 const fs = require("fs");
-const ip = require("ip");
-const prompt = require("prompt-sync")();
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const today = new Date();
 
@@ -19,14 +21,20 @@ let formattedDay = dd + "/" + mm + "/" + yyyy;
 let formattedHour = hour + ":" + min + ":" + sec;
 
 let opSistem = os.type() + " " + os.release();
+let netInfo = os.networkInterfaces();
+let ip = Object.values(netInfo)[0][0].address;
 
-let fileName = prompt("FileName: ");
-
-fs.createWriteStream(fileName).write(
-  `File created : ${formattedDay} ${formattedHour}
+let content = `File created : ${formattedDay} , ${formattedHour}
   
 OS: ${opSistem}
 CPU: ${os.cpus()[0].model}
-IP: ${ip.address()}
-  `
-);
+IP: ${ip}
+  `;
+
+readline.question("FileName: ", (fileName) => {
+  fs.appendFile(fileName, content, function (err) {
+    if (err) throw err;
+    console.log("Done!");
+  });
+  readline.close();
+});
