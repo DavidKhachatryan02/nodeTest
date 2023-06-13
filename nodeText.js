a336489eff43c82d73347594b7900b7412e9d41f
David Khachatryan <davo-davo-dav@bk.ru>
2023-06-13 20:04:22 +0400

    Added my project

---
+++ nodeText.js
@@ -0,0 +1,29 @@
+const os = require("os");
+const fs = require("fs");
+const ip = require("ip");
+
+const today = new Date();
+
+const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1;
+let dd = today.getDate();
+let min = today.getMinutes();
+let hour = today.getHours();
+let sec = today.getSeconds();
+
+if (dd < 10) dd = "0" + dd;
+if (mm < 10) mm = "0" + mm;
+
+let formattedDay = dd + "/" + mm + "/" + yyyy;
+let formattedHour = hour + ":" + min + ":" + sec;
+
+let opSistem = os.type() + " " + os.release();
+
+fs.createWriteStream("text.txt").write(
+  `File created : ${formattedDay} , ${formattedHour}
+  
+OS: ${opSistem}
+CPU: ${os.cpus()[0].model}
+IP: ${ip.address()}
+  `
+);
